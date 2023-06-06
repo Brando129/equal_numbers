6cd7627172592438b52a9b8c232332d61ce1337a
Brandon Colangelo <brandoncancode@gmail.com>
2023-06-06 08:47:11 -0400

    Inital Commit

---
+++ index.py
@@ -0,0 +1,9 @@
+# Create a function that returns true when num1 is equal to num2; otherwise return false.
+
+def equal_numbers(num_one, num_two):
+    if num_one == num_two:
+        return True
+    else:
+        return False
+
+print(equal_numbers(12,11))
